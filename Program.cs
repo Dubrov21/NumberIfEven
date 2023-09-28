7b0b8693b07008c1b91fc60286c4b94f758a0672
Dubrov21 <vald5red@yandex.ru>
2023-09-28 20:56:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NumberIfEven
+{
+    class Answer
+    {
+        static void CheckIfEven(int number)
+        {
+            bool isEven;
+            isEven = (number % 2 == 0) ? true : false;
+            if(isEven)
+            {
+                Console.WriteLine("Число " + number + " четное");
+            }
+            else
+            {
+                Console.WriteLine("Число " + number + " нечетное");
+            }
+            
+        }
+    
+        static public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int number;
+
+            if(args.Length >= 1)
+            {
+                number = int.Parse(args[0]);
+            }
+            else
+            {
+                number = 6;
+            }
+            CheckIfEven(number);
+        }
+    }
+
+}
